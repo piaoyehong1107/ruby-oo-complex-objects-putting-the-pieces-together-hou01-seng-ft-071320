8f41f3cbbadd99822fa99f25314a5d4136ba2efc
Yehong Piao <piaoyehong1107@gmail.com>
2020-07-15 21:34:20 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,12 @@
-# Make your shoe class here!
+class Shoe
+  def initialize(brand)
+    @brand = brand
+  end
+  attr_accessor:author , :page_count, :genre
+  attr_reader:brand
+  
+  def turn_page
+  puts "Flipping the page...wow, you read fast!"
+  end
+  
+end# Make your shoe class here!
