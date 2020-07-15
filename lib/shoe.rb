7a76ff00c1714da3725847889f1ad9a9b6eede94
Yehong Piao <piaoyehong1107@gmail.com>
2020-07-15 21:34:37 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   def initialize(brand)
     @brand = brand
   end
-  attr_accessor:color , :size, :material,:
+  attr_accessor:color , :size, :material,:condition
   attr_reader:brand
   
   def turn_page
